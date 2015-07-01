4b1d0b173ea6eba2b9f9b69a689ab7ef0ae0af5d
jkuharev <kuharev@uni-mainz.de>
2015-07-01 17:08:46 +0200

    missing value statistics are now calculated after removing low intensities

--- inst/scripts/lfqbench.proc.r
+++ inst/scripts/lfqbench.proc.r
@@ -36,29 +36,20 @@ processData = function( DocSet )
   ################################################################################
   
   ################################################################################
-  # calculate missing value statistics
-  calHistNAsWithSpecies <- function(species){
-      sp_hist = hist(csvNumNAs[csvSpecies == species],  breaks=seq(-1, ncol(csvAmounts)), include.lowest=T,  plot=F  )
-      return(sp_hist$counts)
-  }
-  csvNumNAs = apply(csvAmounts,1, function(row) sum(is.na(row)))
-  numNAs_histograms = as.data.frame(sapply(cfg$AllSpeciesNames, calHistNAsWithSpecies)) 
-  numNAs_histograms = cbind( seq(0, ncol(csvAmounts) ), numNAs_histograms)
-  names(numNAs_histograms)[1] = c("numNAs")
-  #row.names(numNAs_histograms) = seq(0, ncol(csvAmounts) - 1)
-  ################################################################################
-  
-  ################################################################################
   # disable low amounts
   csvAmounts[csvAmounts < cfg$MinProteinAmount] = NA
   ################################################################################
   
   ################################################################################
-  # add more info to missing value statistics
+  # calculate missing value statistics
+  calHistNAsWithSpecies <- function(species){
+    sp_hist = hist(csvNumNAs[csvSpecies == species],  breaks=seq(-1, ncol(csvAmounts)), include.lowest=T,  plot=F  )
+    return(sp_hist$counts)
+  }
   csvNumNAs = apply(csvAmounts,1, function(row) sum(is.na(row)))
-  numNAs_histograms_after_dis_low_amounts = as.data.frame(sapply(cfg$AllSpeciesNames, calHistNAsWithSpecies)) 
-  names(numNAs_histograms_after_dis_low_amounts) = paste(names(numNAs_histograms_after_dis_low_amounts), "lowAmountsAsNA",sep="_")
-  numNAs_histograms = cbind( numNAs_histograms, numNAs_histograms_after_dis_low_amounts)
+  numNAs_histograms = as.data.frame(sapply(cfg$AllSpeciesNames, calHistNAsWithSpecies)) 
+  numNAs_histograms = cbind( seq(0, ncol(csvAmounts) ), numNAs_histograms)
+  names(numNAs_histograms)[1] = c("numNAs")
   ################################################################################
   
   ################################################################################
