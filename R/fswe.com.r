5c1be7fd246ec7d0a1f0a1e08069ae7c04d0a54f
mesontau <mesontau+github@gmail.com>
2016-02-11 18:51:09 +0100

    experiment simulator

--- R/fswe.com.r
+++ R/fswe.com.r
@@ -86,7 +86,6 @@ guessOrganism <- function(proteinid, species){
 #' @param values the vector with the values. 
 #' @param n number of top N values to be summed. 
 #' @param minimum minimum number of valid (not NA) values allowed in order to return a value.
-#' 
 sum_top_n <- function(values, n, minimum = 1){
   if(length(which(!is.na(values))) < minimum) {return (NA)}
   if (n > length(values)) n <- length(values)
@@ -179,4 +178,158 @@ guessExperiment_wide <- function(exp, varnames){
     # varnames: column names of the data frame
     any(sapply(exp, grepl, varnames, ignore.case = T))
 }
+
+getProteinKeys <- function(pepsProtein, suffix){
+    proteinKeys = character()
+    for(i in 1:length(pepsProtein)){
+        proteinKeys = c(proteinKeys, rep( paste0(suffix, i), pepsProtein[i]))    
+    }     
+    return(proteinKeys)
+} 
+
+getPeptideKeys <- function(pepsProtein, proteinKeys) {
+    pepKeys = character()
+    for(i in 1:length(pepsProtein)){
+        pepKeys = c( pepKeys, paste0("_", 1:pepsProtein[i]))
+    }
+    pepKeys = paste0(proteinKeys, pepKeys)
+    return(pepKeys)
+}
+
+getPeptideIntensities <- function(peptidesProtein, AbsoluteIntensityProtein){
+    PeptideIntensities = numeric()
+    for(i in 1:length(peptidesProtein)){
+        # plot(1:25, sort( rlnorm(25, meanlog = 20, sd = 2), decreasing = T))
+        # Simulate the total intensity of each peptide of the protein -- we use a logarithmic distribution
+        PeptideInt_i =rlnorm(peptidesProtein[i], meanlog = AbsoluteIntensityProtein[i], sdlog = 1) # TODO: sdlog ??
+        PeptideIntensities = c(PeptideIntensities, PeptideInt_i)
+    }
+    return(PeptideIntensities)
+}
+
+#' FSWE.simExperiment
+#' 
+#' This function simulates a complete experiment that can be read by FSWE.
+#' 
+#' @param numReplicates number of replicates of the experiment (same number for samples A and B)
+#' @param species vector with species tag names. Example: species = c("HUMAN", "YEAST", "ECOLI")
+#' @param speciesRatios vector with expected log ratios for each species. Example: speciesRatios = c(0.0, 1.0, -2.0)
+#' @param stdDevRatios biological/prep variation. Example: stdDeviations = c(0.05, 0.1, 0.1)
+#' @param numProteinsSpecies vector with number of proteins simulated per species. Example: numProteins = c(2000, 1500, 1000)
+#' @param peptidesPerProtein vector with the average number of peptides per protein simulated. Example: peptidesPerProtein = c(10, 8, 5)
+#' @param intDistribution vector containing mean and sd values defining the protein distribution intensities desired. Example: intDistribution = c(10.0, 5.0) 
+#' @param stdDeviationFactorMS standard deviation factor (0 to 1) due to MS. Example: stdDeviationFactorMS = 0.03
+#' @param MissingValuesFactor factor of missing values in the experiment. They are intensity-dependant.
+#' @export
+FSWE.simExperiment <- function(numReplicates, 
+                               species, 
+                               speciesRatios, 
+                               stdDevRatios, 
+                               numProteinsSpecies, 
+                               peptidesPerProtein, 
+                               intDistribution,
+                               stdDeviationFactorMS,
+                               BackgroundSignalLevel,
+                               MissingValuesFactor,
+                               SignalNoiseFactor){
+    
+    pepsProtein = numeric()
+    proteinKeys = character()
+    AbsIntProtein = numeric()
+    
+    experiment <- data.frame(peptideKeys = character(),
+                                proteinKeys = character(),
+                                speciesKeys = character())
+    
+    for(i in 1:(2*numReplicates)){
+        varname = paste0("V", i)
+        experiment[varname] = numeric()
+    }
+        
+    
+    for(i in 1:length(species)){
+        #i = 1
+        # Simulate the number of peptides per protein
+        pepsProtein = rpois(numProteinsSpecies[i], peptidesPerProtein[i])
+        pepsProtein[pepsProtein == 0] = 1  # Fix at the case no peptides assigned to a protein
+        numPeps = sum(pepsProtein)
+        # Assign to each peptide a protein key
+        proteinKeys = getProteinKeys(pepsProtein, paste0("_",species[i]))
+        peptideKeys = getPeptideKeys(pepsProtein, proteinKeys)
+        speciesKeys = rep(species[i], numPeps)
+        # Simulate the total intensity of each protein
+        AbsIntProtein = rnorm(numProteinsSpecies[i], mean = intDistribution[1], sd = intDistribution[2])
+        # Simulate the total intensity of each peptide of the protein -- we use a logarithmic distribution
+        AbsPepInt = getPeptideIntensities(pepsProtein, AbsIntProtein)
+        minAbsPepInt = min(AbsPepInt)
+        maxAbsPepInt = max(AbsPepInt)
+        deltaAbsPepInt = maxAbsPepInt - minAbsPepInt 
+        # Simulate peptide ratios according to species ratios
+        logs <- rnorm( n = numPeps, mean = speciesRatios[i], sd = stdDevRatios[i])  # * (AbsPepInt / max(AbsPepInt)) )
+        
+        # Calculate A and B quantities and write them into a data frame
+        AInt <- AbsPepInt / (1 + 2^logs)
+        BInt <- AInt / (2^logs)
+        # log2AB = log(AInt/BInt, 2)
+        
+        # Generate replicates in the experiment
+        rnormfunct <- function(meanInt, sd, n){
+            rnorm(n = n, mean = meanInt, sd = sd * meanInt) # This defines the instrument error (between replicates)
+        }
+
+        # Replicates for A and B        
+        dfA = as.data.frame(t(sapply(AInt, rnormfunct, stdDeviationFactorMS, numReplicates)))
+        dfB = as.data.frame(t(sapply(BInt, rnormfunct, stdDeviationFactorMS, numReplicates)))
+        
+        # add random values (normal distributed) to all replicates, regardless the intensity
+        rndMean = 0
+        rndSD = (2 ^ intDistribution[1]) * SignalNoiseFactor 
+        for(rep in 1:numReplicates){
+            rndNoiseA = rnorm(length(AInt), mean = rndMean, sd = rndSD)
+            rndNoiseB = rnorm(length(BInt), mean = rndMean, sd = rndSD)
+            dfA[, rep] = dfA[, rep] + rndNoiseA
+            dfB[, rep] = dfB[, rep] + rndNoiseB
+            dfA[ dfA < BackgroundSignalLevel] = 0.0
+            dfB[ dfB < BackgroundSignalLevel] = 0.0
+            
+        }
+        
+        df = cbind(peptideKeys, proteinKeys, speciesKeys, dfA, dfB)
+        
+        experiment = rbind(experiment, df)
+    }
+    
+    names(experiment) = c("peptide", "protein", "species", paste0("quant_A", 1:numReplicates) , paste0("quant_B", 1:numReplicates) )
+    experiment_bcp <- experiment
+    # Entry missing values
+    numericCols <- sapply(experiment, is.numeric)
+    keyCols <- names(numericCols[numericCols == FALSE])
+    numericCols <- names(numericCols[numericCols == TRUE])
+    
+    # We entry missing values to each of the replicates: a total of length(replicate) * MissingValuesFactor
+    # The probobility a peptide (peak) is taken as missing value is: min( MVP + 1 / (Signal/Noise) , 1- MVP )
+    MVP = 0.01
+    expLength = nrow(experiment)
+    numNAsPerReplicate = as.integer(expLength * MissingValuesFactor)
+    for(repl in numericCols){
+        # repl = numericCols[1]
+        currRepl = experiment[, repl]
+        experiment_tmp <- experiment %>%
+                        mutate(MVprob = 1/(currRepl/BackgroundSignalLevel)) %>% rowwise() %>%
+                        mutate(MVprob = MVP + min(MVprob, 1-MVP)) %>% ungroup()
+        
+        naList <- sample.int(n = expLength, size = numNAsPerReplicate, replace = F, prob = experiment_tmp$MVprob)
+        experiment[naList, repl] <- NA
+        
+    }
+    
+    experiment[experiment < BackgroundSignalLevel ] = NA
+    
+    experiment$charge = 2
+    
+    
+    return(experiment) 
+}
+
+
 
---
+++ inst/scripts/ExperimentSimulation.R
@@ -0,0 +1,73 @@
+library(LFQbench)
+library(readr)
+
+set.seed(1234)
+
+# General LFQbench settings =======================================================
+srcDir = "/Users/napedro/tmp_data/LFQbench/simulator"
+expFile = "test_PeptideSummary.tsv"
+
+dataSets = data.frame(
+    "Test"=c("A1", "A2", "A3",   # A
+                     "B1", "B2", "B3")   # B , 
+    ,row.names=c("A1", "A2", "A3", "B1", "B2", "B3")
+)
+
+# Define the species tags of the protein entries in your experiment
+speciesTags = list(HUMAN = "_HUMAN", YEAST = "_YEAS", ECOLI = "_ECOLI") 
+
+
+# For the simulator ===============================================================
+numReplicates = 3
+numProteinsSpecies = c(2000, 1500, 1000)
+peptidesPerProtein = c(3, 3, 3)
+species = c("HUMAN", "YEAST", "ECOLI")
+speciesRatios = c(0.0, 1.0, -2.0)
+stdDevRatios = c(0.001, 0.001, 0.001)
+intDistribution = c(13.0, 1.5)
+stdDeviationFactorMS = 0.03
+BackgroundSignalLevel = 2^1 #2^14
+MissingValuesFactor = 0.01
+SignalNoiseFactor = 0.0
+
+# =================================================================================
+
+mySimExp <- FSWE.simExperiment(numReplicates = numReplicates,  
+                   numProteinsSpecies = numProteinsSpecies, 
+                   species = species, 
+                   speciesRatios = speciesRatios, 
+                   stdDevRatios = stdDevRatios, 
+                   peptidesPerProtein = peptidesPerProtein, 
+                   intDistribution = intDistribution,
+                   stdDeviationFactorMS = stdDeviationFactorMS,
+                   BackgroundSignalLevel = BackgroundSignalLevel,
+                   MissingValuesFactor = MissingValuesFactor,
+                   SignalNoiseFactor = SignalNoiseFactor)
+
+#write.csv2(mySimExp, file = file.path(srcDir, expFile) , sep = "\t", na = "NA", row.names = F, col.names = T)
+
+write_tsv(mySimExp, path = file.path(srcDir, expFile), na = "NA", col_names = T)
+
+LFQbench.initConfiguration()
+FSWE.initConfiguration( dataSets, speciesTags )
+FSWE.addSoftwareConfiguration("test", 
+                              input_format = "wide", 
+                              input.extension = "*.tsv$", 
+                              nastrings = "NA", 
+                              protein_input = F, 
+                              quantitative.var = "quant",
+                              quantitative.var.tag = "quant", 
+                              protein.var = "protein", 
+                              sequence.mod.var = "peptide", 
+                              charge.var = "charge")
+FSWE.switchSoftwareConfiguration("test")
+
+LFQbench.setDataRootFolder( srcDir, createSubfolders = T )
+
+FSWE.generateReports(experimentFile = expFile, 
+                     plotHistogram = T, 
+                     plotHistNAs = F, 
+                     reportSequences = F, 
+                     keep_original_names = T)
+
+nix = LFQbench.batchProcessRootFolder()
